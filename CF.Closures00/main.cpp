b201267a962580a62a53563b1c11455bf93ae8a9
Alan Sampson <alansamps@gmail.com>
2021-01-07 15:52:24 -0800

    Initial Commit

---
+++ CF.Closures00/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CF.Closures00
+//
+//  Created by Alan Sampson on 1/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
